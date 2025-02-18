3b0dc3d62570b19ec565ca3c25c36ae34579f2f6
jaykoo <jaykoo.9278@gmail.com>
2025-02-19 00:11:49 +0800

    1.0

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,21 @@
 import _ from "lodash";
+import "./style.css";
+import Img from "./image.png";
+import data from "./data.xml";
+import notes from "./data.csv";
 function component() {
   const element = document.createElement("div");
 
   // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "大撒大撒大撒大撒"], " ");
+
+  element.classList.add("hello");
+
+  const img = new Image();
+  img.src = Img;
+  element.appendChild(img);
+  console.log(data);
+  console.log(notes);
 
   return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,7 +3,33 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jepg|jpg|gif|svg)$/i,
+        // todo
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eto|ttf|otf)$/i,
+        // todo
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+    ],
   },
 };
